b920f2cd93cfd22b26d14fa72d6c96f8dfc975cf
jessny <jessnyjoseph@gmail.com>
2021-03-01 19:16:36 -0600

    added home page text

--- client/src/pages/Home/home.js
+++ client/src/pages/Home/home.js
@@ -1,10 +1,13 @@
 import React from "react";
 // import Header from "../../components/Header/header"
+import "./home.css";
 
 const Home = () => {
     return (
         <div>
-            <p>hi</p>
+            <h1>Hi, I'm Jessny.</h1>
+            <p>I am a web developer interested in the convergence between biological systems and emerging
+            technologies. Feel free to look at my most recent projects.</p>
         </div>
     );
 }
