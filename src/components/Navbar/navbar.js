f9388758a5ac0c866d368b8d89a00474ab06e5aa
jessny <jessnyjoseph@gmail.com>
2021-03-16 15:43:54 -0500

    adding elements to nav hamburger

--- src/components/Navbar/navbar.js
+++ src/components/Navbar/navbar.js
@@ -1,30 +1,30 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
     return (
         <nav className="navbar is-transparent">
             <div className="navbar-brand">
-                <div className="navbar-burger" data-target="navbarExampleTransparentExample">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </div>
+                <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarExampleTransparentExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
             </div>
 
             <div id="navbarExampleTransparentExample" className="navbar-menu">
                 <div className="navbar-end">
-                <Link to="/" class="navbar-item">
-                            Home
+                    <Link to="/" class="navbar-item">
+                        Home
                         </Link>
-                        <Link to="/projects" class="navbar-item">
-                            Projects
+                    <Link to="/projects" class="navbar-item">
+                        Projects
                         </Link>
-                        <Link to="/about" class="navbar-item">
-                            About Me
+                    <Link to="/about" class="navbar-item">
+                        About Me
                         </Link>
-                        <Link to="/contact" class="navbar-item" >
-                            Contact
+                    <Link to="/contact" class="navbar-item" >
+                        Contact
                         </Link>
                 </div>
             </div>
